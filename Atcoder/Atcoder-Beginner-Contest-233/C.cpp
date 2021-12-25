7e1877268262121eec8b22d89979cabb36157eb9
wtcode-git <95998330+wtcode-git@users.noreply.github.com>
2021-12-25 19:20:22 +0530

    Update and rename tmp to C.cpp

---
+++ Atcoder/Atcoder-Beginner-Contest-233/C.cpp
@@ -0,0 +1,97 @@
+/*
+    author:ujwalll
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); i--)
+#define nx cout << "\n";
+const int INF = 1 << 29;
+const int MOD = 1e9 + 7;
+#define pp pair<ll, ll>
+#define ppi pair<int, int>
+typedef long long int ll;
+#define pri(x) printf("%d ", x);
+#define prl(x) printf("%lld ", x);
+#define fi first;
+#define se second;
+#define pb push_back;
+#define all(v) v.begin(), v.end()
+#define minimum 1
+#define maximum 1e5 
+#define random minimum + (rand() % static_cast<int>(maximum - minimum + 1))
+
+bool isPowerOfTwo(ll x){
+    return x && (!(x & (x - 1)));
+}
+void fastio(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+long long binpow(long long a, long long b){
+    long long res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll ncr(int n, int r) {
+    if (n < 0 || r < 0 || r > n)
+        return 0;
+
+    ll ans = 1;
+    for (int i = 1; i <= r; i++) {
+        ans *= (n + 1 - i);
+        ans /= i;
+    }
+
+    return ans;
+}
+const int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[] = {0, -1, 0, 1, 1, -1, -1, 1};
+////////////////////////////////////////////////////////////////////
+
+
+vector<vector<ll>> v;
+ll n,x;
+ll solve(ll i, ll x){
+    if(i == n){
+        return (x == 1);
+    }
+    ll ans = 0;
+    // jth ball in my ith bag
+    rep(j, v[i].size()){
+        if(x % v[i][j] == 0)
+        ans = ans + solve(i + 1, x / v[i][j]);
+    }
+    return ans;
+}
+void test_case(){
+    cin>>n>>x;
+    rep(i,n){
+        ll l;cin>>l;
+        vector<ll> in(l);
+        rep(j,l) cin>>in[j];
+        v.push_back(in);
+    }
+    cout << solve(0, x);
+}
+int main(){
+    fastio();
+    int tc = 1;
+    // cin>>tc;
+    while(tc--){
+        test_case();
+        done:;
+    }
+    return 0;
+}
